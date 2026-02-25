ad05089f9a59bcffacede707513647a5fffdb576
zyonetsuaniki <zyonetsuaniki@gmail.com>
2026-02-25 14:04:05 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,18 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-      <h1 className={styles.title}>
-        クイズ大会
-      </h1>
-      <Image
-        src="/QR.png"
-        alt="QR"
-        width={300}
-        height={300}
-      />
+        <h1 className={styles.title}>
+          クイズ大会
+        </h1>
+
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Image
+          src="/QR.png"
+          alt="QR"
+          width={500}
+          height={500}
+        />
+        </div>
       </div>
     </main>
   );
