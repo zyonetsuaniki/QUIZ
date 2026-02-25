09952fe5b525f0ab479714920ecfb2451e17ea45
zyonetsuaniki <zyonetsuaniki@gmail.com>
2026-02-25 13:58:56 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+import Image from "next/image";
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>
       <h1 className={styles.title}>
         クイズ大会
       </h1>
+      <Image
+        src="/QR.png"
+        alt="QR"
+        width={300}
+        height={300}
+      />
+      </div>
     </main>
   );
 }
