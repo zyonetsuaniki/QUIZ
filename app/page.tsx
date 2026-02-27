cba1b851578b74a0989e1cc662504adfb61eb1e5
zyonetsuaniki <zyonetsuaniki@gmail.com>
2026-02-27 22:24:51 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,30 @@ import styles from './page.module.css';
 
 export default function Home() {
 
-  const openAdminAndScreen = () => {
+  const openAdminAndScreen1 = () => {
     // adminページを新しいタブで開く
     window.open("/admin", "_blank");
-
+  };
+  const openAdminAndScreen2 = () => {
     // screenページを新しいタブで開く
     window.open("/screen", "_blank");
   };
-
   return (
     <>
        <>
         <main className={styles.main}>
           <div className={styles.topscreen}>
             <button
-              onClick={openAdminAndScreen}
+              onClick={openAdminAndScreen1}
               className={styles.startButton}
             >
-              管理画面とスクリーンを開く
+              管理画面を開く
+            </button>
+            <button
+              onClick={openAdminAndScreen2}
+              className={styles.startButton}
+            >
+              スクリーンを開く
             </button>
           </div>
         </main>
