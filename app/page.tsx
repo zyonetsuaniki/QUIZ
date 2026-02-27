21b4501c571e2c2ad8dc98a204b8811ffd79ef00
zyonetsuaniki <zyonetsuaniki@gmail.com>
2026-02-27 20:23:00 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 "use client";
 
+import styles from './page.module.css';
 
 export default function Home() {
+
+  const openAdminAndScreen = () => {
+    // adminページを新しいタブで開く
+    window.open("/admin", "_blank");
+
+    // screenページを新しいタブで開く
+    window.open("/screen", "_blank");
+  };
+
   return (
-    <main >
-      
-    </main>
+    <>
+       <>
+        <main className={styles.main}>
+          <div className={styles.topscreen}>
+            <button
+              onClick={openAdminAndScreen}
+              className={styles.startButton}
+            >
+              管理画面とスクリーンを開く
+            </button>
+          </div>
+        </main>
+      </>
+    </>
   );
 }
